cf195f47d56c56d1a3e61688bcfc9dbb55463d54
jyf <107237615@qq.com>
2025-04-03 22:31:39 +0800

    fix it

--- app/src/main/java/com/example/demo1/app/test/stringbuilderandbuffer/TestController.java
+++ app/src/main/java/com/example/demo1/app/test/stringbuilderandbuffer/TestController.java
@@ -21,17 +21,27 @@ public class TestController {
         StringBuffer stringBuffer = new StringBuffer();
         ExecutorService executorService = Executors.newCachedThreadPool();
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
-        for (int i = 0; i < clientTotal ; i++) {
+
+        for (Integer integer : queue) {
             executorService.execute(() -> {
                 try {
-//                    stringBuffer.append(queue.remove());
-                    stringBuffer.append(1);
+                    stringBuffer.append(integer);
                 } catch (Exception e) {
                     log.error("exception", e);
                 }
                 countDownLatch.countDown();
             });
         }
+
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         return stringBuffer.length();
     }
 
@@ -39,23 +49,26 @@ public class TestController {
     public Integer jointBuilder() {
         //先生成一个10000个1的列表
         int clientTotal = 10000;
-        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
-        for (int i = 0; i < clientTotal; i++) {
-            queue.add(1);
-        }
         StringBuilder stringBuilder = new StringBuilder();
         ExecutorService executorService = Executors.newCachedThreadPool();
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
         for (int i = 0; i < clientTotal ; i++) {
             executorService.execute(() -> {
                 try {
-//                    stringBuilder.append(queue.remove());
                     stringBuilder.append(1);
                 } catch (Exception e) {
                     log.error("exception", e);
                 }
                 countDownLatch.countDown();
             });
+        }
+
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
 
         return stringBuilder.length();
